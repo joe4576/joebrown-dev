1cfbfa2d479d641a72b2a96e1179b57317b85d5e
Joe Brown <joebrown.4576@gmail.com>
2024-09-21 23:15:58 +0100

    Add tenure to jobs

--- src/main.js
+++ src/main.js
@@ -28,3 +28,31 @@ mobileItems.forEach((item) => {
     closeMobileMenu();
   });
 });
+
+const calculateTimeSinceDateInclusive = (date) => {
+  const now = new Date();
+  const inputDate = new Date(date);
+
+  const years = now.getFullYear() - inputDate.getFullYear();
+  const months = now.getMonth() - inputDate.getMonth() + 1;
+
+  return { years, months };
+};
+
+const setCurrentJobTenure = () => {
+  const tenure = document.getElementById("current-job-tenure");
+  const startDate = tenure?.dataset.startDate;
+
+  if (!startDate) {
+    return;
+  }
+
+  const { years, months } = calculateTimeSinceDateInclusive(startDate);
+
+  const duration =
+    years === 0 ? `${months} mos` : `${years} yrs, ${months} mos`;
+
+  tenure.textContent += `(${duration})`;
+};
+
+setCurrentJobTenure();
